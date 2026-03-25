b486e2c9aebc8e47b3dadd4308a5c46efc0f92d8
ella <randomgive98@gmail.com>
2026-03-25 15:14:50 +0100

    08 and 09 fix

--- submission/08.sh
+++ submission/08.sh
@@ -2,15 +2,14 @@
 # The transaction fee should be 20,000 satoshis, then the rest of the amount should be sent to the address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP
 # Use the UTXOs from the transaction below
 # raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
-RAW_TX="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
-# Recipient and fee
-RECIPIENT="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-FEE_SATS=20000
-TXID=$(bitcoin-cli -regtest decoderawtransaction "$RAW_TX" | jq -r '.txid')
-TOTAL_SATS=$(bitcoin-cli -regtest decoderawtransaction "$RAW_TX" | jq '[.vout[].value*100000000|floor] | add')
-SEND_SATS=$((TOTAL_SATS - FEE_SATS))
-SEND_BTC=$(awk -v s="$SEND_SATS" 'BEGIN { printf "%.8f", s/100000000 }')
-INPUTS=$(jq -n --arg txid "$TXID" '[{"txid":$txid,"vout":0},{"txid":$txid,"vout":1}]')
-OUTPUTS=$(jq -n --arg addr "$RECIPIENT" --arg amount "$SEND_BTC" '{($addr): ($amount|tonumber)}')
-bitcoin-cli -regtest -named createrawtransaction inputs="$INPUTS" outputs="$OUTPUTS"
+txid_1=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.txid')
+total_amount=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout | .[] | .value' | awk '{s+=$1} END {print int(s*100000000 + 0.5)}')
+amount=$(printf "%.0f\n" $(echo "$total_amount-20000" | bc -l))
+vout_1=0
+vout_2=1
+address_to_send="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+amount_btc=$(printf "%.8f" $(echo "scale=8; $amount / 100000000" | bc -l))
+
+bitcoin-cli -regtest -named createrawtransaction inputs='''[{"txid": "'$txid_1'", "vout":'$vout_1'}, {"txid": "'$txid_1'", "vout": '$vout_2'}]''' outputs='''[{"'$address_to_send'":'$amount_btc'}]'''
